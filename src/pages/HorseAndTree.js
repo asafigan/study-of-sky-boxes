c75f3b21e199a66073442f43626e0501f1f7e93a
Andrew Safigan <asafigan@gmail.com>
2018-02-23 00:00:36 -0500

    added skybox to horse and tree

--- src/pages/HorseAndTree.js
+++ src/pages/HorseAndTree.js
@@ -10,24 +10,19 @@ export default function HorseAndTree({history}) {
         <Entity primative="a-asset-item" id="horseObj" src={horseObj}/>
         <Entity primative="a-asset-item" id="treeObj" src={treeObj}/>
       </Entity>
-      <Entity class="clickable" text-geometry="value: Home" events={{
-        click: () => {
-          history.push("/")
-        },
-        }} material-portal="scene: sky" position="-2 0 -5" scale="2 2 2"/>
+
       <Entity position="1 0 -5">
         <Entity scale="0.2 0.2 0.2" position="0 0 -1.5">
-          <Entity primitive="a-obj-model" position="13 0.8 -7" rotation="85 0 0" src="#treeObj"/>
+          <Entity primitive="a-obj-model" material-portal="scene: sky" position="13 0.8 -7" rotation="85 0 0" src="#treeObj"/>
         </Entity>
         <Entity scale="0.6 0.6 0.6" >
           <Entity rotation="0 90 -102" position="-1 1 0">
-            <Entity primitive="a-obj-model" src="#horseObj" position="0 -6 0"/>
+            <Entity primitive="a-obj-model" material-portal="scene: forest" src="#horseObj" position="0 -6 0"/>
           </Entity>
         </Entity>
       </Entity>
 
-
-      <Entity primitive="a-sky" material-portal="scene: forest"/>
+      <Entity primitive="a-sky" color="blue"/>
       <Entity primitive="a-camera">
         <Entity primitive="a-cursor" raycaster="objects: .clickable"/>
       </Entity>
