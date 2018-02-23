afa167a36e04e198d9a256b61e394202b804f101
Andrew Safigan <asafigan@gmail.com>
2018-02-22 23:54:46 -0500

    positioned tree

--- src/pages/HorseAndTree.js
+++ src/pages/HorseAndTree.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import {Entity} from 'aframe-react'
 import horseObj from '../models/horse.obj'
+import treeObj from '../models/tree.obj'
 
 export default function HorseAndTree({history}) {
   return (
     <Entity>
-      <a-resources>
-        <a-asset-item id="horseObj" src={horseObj}/>
-      </a-resources>
+      <Entity primative="a-resources">
+        <Entity primative="a-asset-item" id="horseObj" src={horseObj}/>
+        <Entity primative="a-asset-item" id="treeObj" src={treeObj}/>
+      </Entity>
       <Entity class="clickable" text-geometry="value: Home" events={{
         click: () => {
           history.push("/")
         },
         }} material-portal="scene: sky" position="-2 0 -5" scale="2 2 2"/>
-      <Entity scale="0.6 0.6 0.6" position="0 0 -5">
-        <Entity rotation="0 90 -102" position="0 1 0">
-          <Entity primitive="a-obj-model" src="#horseObj"   position="0 -6 0"/>
+      <Entity position="1 0 -5">
+        <Entity scale="0.2 0.2 0.2" position="0 0 -1.5">
+          <Entity primitive="a-obj-model" position="13 0.8 -7" rotation="85 0 0" src="#treeObj"/>
+        </Entity>
+        <Entity scale="0.6 0.6 0.6" >
+          <Entity rotation="0 90 -102" position="-1 1 0">
+            <Entity primitive="a-obj-model" src="#horseObj" position="0 -6 0"/>
+          </Entity>
         </Entity>
       </Entity>
+
 
       <Entity primitive="a-sky" material-portal="scene: forest"/>
       <Entity primitive="a-camera">
