dca3bf07c72b725bf0bf385ff84998d45449d1e3
Andrew Safigan <asafigan@gmail.com>
2018-02-22 23:22:47 -0500

    added horse to scene

--- src/pages/HorseAndTree.js
+++ src/pages/HorseAndTree.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import {Entity} from 'aframe-react'
+import horseObj from '../models/horse.obj'
 
 export default function HorseAndTree({history}) {
   return (
     <Entity>
       <a-resources>
-
+        <a-asset-item id="horseObj" src={horseObj}/>
       </a-resources>
-      <Entity text-geometry="value: Home" events={{
+      <Entity class=".clickable" text-geometry="value: Home" events={{
         click: () => {
           history.push("/")
         },
         }} material-portal="scene: sky" position="-2 0 -5" scale="2 2 2"/>
+      <Entity scale="0.6 0.6 0.6" position="0 0 -5">
+        <Entity rotation="0 90 -102" position="0 1 0">
+          <Entity primitive="a-obj-model" src="#horseObj"   position="0 -6 0"/>
+        </Entity>
+      </Entity>
+
       <Entity primitive="a-sky" material-portal="scene: forest"/>
       <Entity primitive="a-camera">
-        <Entity primitive="a-cursor"/>
+        <Entity primitive="a-cursor" raycaster="objects: .clickable"/>
       </Entity>
     </Entity>
   )
