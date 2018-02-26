5f585fde717090c91a4ae5602c11cd2edac806bf
Andrew Safigan <asafigan@gmail.com>
2018-02-26 00:27:45 -0500

    added link to github

--- src/App.js
+++ src/App.js
@@ -10,21 +10,27 @@ import Home from './pages/Home'
 import Hallway from './pages/Hallway'
 import Doorways from './pages/DoorWays'
 import NotFound from './pages/NotFound'
-
+import './index.css'
 export default class App extends Component {
   render() {
     return (
-      <Scene>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/horse" component={HorseAndTree}/>
-            <Route exact path="/hallway" component={Hallway}/>
-            <Route exact path="/doorways" component={Doorways}/>
-            <Route component={NotFound}/>
-          </Switch>
-        </Router>
-      </Scene>
+      <div>
+        <a href="https://github.com/asafigan/study-of-sky-boxes" class="hovering-text">
+          github
+        </a>
+        <Scene>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/horse" component={HorseAndTree}/>
+              <Route exact path="/hallway" component={Hallway}/>
+              <Route exact path="/doorways" component={Doorways}/>
+              <Route component={NotFound}/>
+            </Switch>
+          </Router>
+        </Scene>
+      </div>
+
     )
   }
 }
